1c75bf7161f46f5adc5f1f48b2aca5baf1958c97
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-02 13:42:07 +0400

    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- yield/YieldProlog.py
+++ yield/YieldProlog.py
@@ -4,5 +4,25 @@ class Term:
         self.nest = []
         self.attr ={}
         
+    ## iterator
+    def __iter__(self): return self.iter
+    def iter(self):
+        for j in self.nest: yield j
+
+    def __repr__(self): return self.dump()
+    def dump(self): return '%s : %s'% (self.value , self.nest)
         
-print Term(x)
+print Term('x')
+
+def Pers(name):
+    yield 'A'
+    yield 'B'
+    yield 'C'
+    
+class oPers(Term):
+    def __init__(self,V):
+        Term.__init__(self, V)
+        self.nest = ['A','B','C']
+
+for p in oPers('pers'):
+    print p
