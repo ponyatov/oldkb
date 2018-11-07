a8cfa6bc8db33fe4a219f5d8b0b9a23d30527c46
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-07 12:27:28 +0400

    + quote/apply
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- lisp/README.md
+++ lisp/README.md
@@ -2,3 +2,4 @@
 
 * https://habr.com/post/227119/
 * http://norvig.com/lispy.html
+* https://ponyatov.quora.com/%C2%B5Lisp-in-Python
--- lisp/lis.py
+++ lisp/lis.py
@@ -1,18 +1,31 @@
-# Lisp machine
+# yLisp machine
+# https://ponyatov.quora.com/%C2%B5Lisp-in-Python
 
+def quote(args): return args
+
+def apply(args): return args[0] ( args[1] )
+
+# the Lisp magic
 def eval(that):
-    # the magic
-    if isinstance(that,(list,tuple)):
-        # the first element of a list is a processing function
-        fn   = eval( that[0] )
-        # other elements are optional arguments
-        args = [ eval(item) for item in that[1:] ]
-        return fn(args)
     # evaluate atoms as is
-    else: return that
+    if not isinstance(that,(list,tuple)): return that
+    # the first element of a list is a processing function
+    fn   = eval( that[0] )
+    # other elements are optional arguments
+    if fn != quote: args = [ eval(item) for item in that[1:] ]
+    # quote blocks the evaluation
+    else:           args = that[1:]
+    return fn(args)
 
 def plus(args): return sum(args)
 
 print eval([plus,1,2,3])
 print eval((plus,1,2,3))
 print eval((plus, 3, (plus, 2, 10), 5))
+
+print eval([quote,1,2,3])
+
+print eval([quote,1,[quote,2,3]])
+
+# print eval((plus, (quote, 1, 2, 3)))
+print eval((apply, plus, (quote, 1, 2, 3)))
