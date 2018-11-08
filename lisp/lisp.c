d25d3c31e8954d6f44d217e4c37dad2765982c22
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-08 10:54:39 +0400

    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- lisp/lisp.c
+++ lisp/lisp.c
@@ -10,11 +10,25 @@ void yyerror(char* msg) {
 
 int main(int argc, char *argv[]) {
 	int i;
+	/* process files from command line */
 	for (i=1; i<argc; i++) {
 		fprintf(stderr,"\ninput: %s\n",argv[i]);
 		yyin = fopen(argv[i],"r"); assert(yyin);
 		yyparse();
 		fclose(yyin);
+	}
+	/* will run only while no files given in command line */
+	if (argc == 1) {
+		printf("\n%s\nPress Ctrl-D to exit\n",URL);
+		/* REPL */
+		while (1) {
+			char *input = readline("\nlisp> ");
+			add_history(input);
+			printf("\ninput: %s\n",input);
+			if (input==NULL) exit(0);		/* Ctrl-D pressed */
+			yyparse();
+			free(input);
+		}
 	}
 	return 0;
 }
--- lisp/lisp.h
+++ lisp/lisp.h
@@ -4,9 +4,14 @@
 #ifndef _H_LISP
 #define _H_LISP
 
+#define URL "https://github.com/ponyatov/kb/tree/dev/lisp"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
 
 extern int yylex();
 extern int yylineno;
