71234bf362db12b0ca3f8c380dd26110e3d96748
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-09 12:39:28 +0400

    + unit parser & variable memory
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- bodik/bodik.py
+++ bodik/bodik.py
@@ -4,24 +4,31 @@
 
 import ply.lex  as lex
 
-tokens = ['sym','num','op','unit']
+tokens = ['sym','int','num','op','unit']
 
 t_ignore = ' \t\r\n'
 
+# operator
 def t_op(t):
     r'[\+\-\*\/\^\=]'
     return t
 
+# number
 def t_num(t):
-    r'[0-9]+(\.[0-9]+)?'
+    r'[0-9]*\.[0-9]+'
     t.value = float(t.value) ; return t
+    
+# integer
+def t_int(t):
+    r'[0-9]+'
+    t.value = int(t.value) ; return t
 
-def t_unit(t):
-    r'kg|m|s'
-    return t
-
+# symbol (variable name)
 def t_sym(t):
     r'[a-zA-Z0-9_]+'
+    # is measurement unit ?
+    if t.value in ['kg','m','s']:
+        t.type = 'unit'
     return t
 
 def t_error(t): raise SyntaxError(t)
@@ -38,9 +45,47 @@ def p_recur(p):
     ' REPL : REPL ex '
     print p[2]
     
-def p_ex_num(p):
-    ' ex : num'
+# variable memory
+M = {}
+
+def p_ex_sym(p):
+    ' ex : sym '
+    try:
+        p[0] = M[p[1]]
+    except KeyError:
+        p[0] = None
+        
+def p_ex_set(p):
+    ' ex : sym op ex '
+    if p[2] != '=': raise SyntaxError(p[2])
+    p[0] = M[p[1]] = p[3] ; print M
+        
+# numeric value
+def p_ex_val(p):
+    ' ex : val'
     p[0] = p[1]
+    
+def p_ex_int(p):
+    ' val : int'
+    p[0] = p[1]
+def p_ex_num(p):
+    ' val : num'
+    p[0] = p[1]
+    
+# value with units
+def p_ex_epsilon(p):
+    ' ex : ex un '
+    p[0] = (p[1],p[2])
+    
+def p_un(p):
+    ' un : unit '
+    p[0] = p[1]
+def p_un_pow(p):
+    ' un : un op int'
+    if p[2] == '^':
+        p[0] = {p[1]:p[3]}
+    else:    
+        p[0] = (p[2],p[1],p[3])
 
 def p_error(p): raise SyntaxError(p) 
 
