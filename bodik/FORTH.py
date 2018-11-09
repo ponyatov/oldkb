500af175bafcf9219954ad1710591f777ec12e31
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-09 09:44:49 +0400

    + bodik
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- bodik/FORTH.py
+++ bodik/FORTH.py
@@ -1,4 +1,4 @@
-# https://www.youtube.com/playlist?list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8
+# object FORTH
 
 import os,sys,time,re,pickle
 
---
+++ bodik/bodik.py
@@ -0,0 +1,2 @@
+# https://www.youtube.com/playlist?list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8
+
