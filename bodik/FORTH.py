60fddee8046c56942a971feea87c4594bd613208
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-09 10:39:09 +0400

    + WORD/FIND/EXECUTE
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- bodik/FORTH.py
+++ bodik/FORTH.py
@@ -22,11 +22,24 @@ class Object:
     # frame[key] = obj            
     def __setitem__(self,key,obj):
         self.attr[key] = obj
+    # frame[key]
+    def __getitem__(self,key):
+        return self.attr[key]
+        
+    # treat nest as stack
+    
     # frame << obj
     def __lshift__(self,obj):
-        self.push(obj)
+        return self.push(obj)
+    # push as stack
     def push(self,obj):
         self.nest.append(obj) ; return self
+    # pop as stack
+    def pop(self):
+        return self.nest.pop()
+    # top element
+    def top(self):
+        return self.nest[-1]
     
     # dumping
     
@@ -55,20 +68,22 @@ class Symbol(Primitive): pass
 class Number(Primitive):
     def __init__(self,N):
         Primitive.__init__(self, float(N))
+    # math
+    def add(self,obj):
+        if isinstance(obj,Number):
+            return Number(self.value + obj.value)
+        else:
+            raise SyntaxError(obj)
+        
+class String(Primitive): pass
 
 class Container(Object): pass
 
 class Stack(Object): pass
 
 class Map(Container):
-    # map << obj
-    def __lshift__(self,obj):
-        if isinstance(obj,Object):
-            self[obj.value] = obj
-        elif callable(obj):
-            self << Fn(obj)
-        else:
-            raise SyntaxError(obj)
+    def __lshift__(self,F):
+        self.attr[F.__name__] = Fn(F)
         
 class Active(Object): pass
 
@@ -76,6 +91,8 @@ class Fn(Active):
     def __init__(self,F):
         Active.__init__(self, F.__name__)
         self.fn = F
+    def __call__(self):
+        self.fn()
 
 S = Stack('data')
 
@@ -86,6 +103,10 @@ W['S'] = S
 
 def BYE(): sys.exit(0)
 W << BYE
+
+def ADD():
+    B = S.pop() ; A = S.pop() ; S.push( A.add(B) )
+W['+'] = Fn(ADD)
 
 import ply.lex as lex
 
@@ -105,16 +126,37 @@ def t_error(t): raise SyntaxError(t)
 
 lexer = lex.lex()
 
-def INTERPRET(SRC):
-    lexer.input(SRC)
+# WORD ( -- obj:token ) get next token from source code stream
+def WORD():
+    token = lexer.token()
+    if token: S << token
+    return token
+W << WORD
+
+# FIND ( token -- callable ) lookup in vocabulary
+def FIND():
+    token = S.pop()
+    try:
+        S.push( W[token.value] ) ; return True
+    except KeyError:
+        S.push(token) ; return False
+    
+# EXECUTE ( callable -- ) run callable
+def EXECUTE():
+    S.pop() ()
+
+# INTERPET ( str -- ) interpret string
+def INTERPRET():
+    lexer.input(S.pop().value)
     while True:
-        token = lexer.token()
-        if not token: break
-        S << token
+        if not WORD(): break
+        if isinstance(S.top(),Symbol):
+            if FIND(): EXECUTE()
+W << INTERPRET
 
 while True:
     print W,S
     try:
-        INTERPRET(raw_input('\nbodik> '))
+        S .push( String ( raw_input('\nbodik> ') )) ; INTERPRET()
     except EOFError:
         BYE()
