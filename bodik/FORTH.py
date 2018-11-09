fac573654da3c8591481a0c6bc92569ad6dececa
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-09 10:04:47 +0400

    + push primitive symbol/number
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- bodik/FORTH.py
+++ bodik/FORTH.py
@@ -1,15 +1,35 @@
-# object FORTH
+# object FORTH works over Marvin Minsky's frame model
 
 import os,sys,time,re,pickle
 
+# base generic unitree object = frame
+# can act as any type wrapper, vector/stack, dict/map/vocabulary = slots
+
 class Object:
+    
     def __init__(self,V):
-        self.type = self.__class__.__name__.lower()
+        # type/class tag, required for PLY parser library
+        self.type  = self.__class__.__name__.lower()
+        # signel value for primitive types from implementation language 
         self.value = V
-        self.attr = {}
-        self.nest = []
+        # slots = associative array keys
+        self.attr  = {}
+        # vector = stack = subtrees
+        self.nest  = []
+
+    # unitree operations 
+
+    # frame[key] = obj            
     def __setitem__(self,key,obj):
         self.attr[key] = obj
+    # frame << obj
+    def __lshift__(self,obj):
+        self.push(obj)
+    def push(self,obj):
+        self.nest.append(obj) ; return self
+    
+    # dumping
+    
     def __repr__(self):
         return self.dump()
     dumped = []
@@ -27,16 +47,25 @@ class Object:
         return '%s<%s:%s>'%(prefix,self.type,self.value)
     def pad(self,N):
         return '\n'+'\t'*N
+    
+class Primitive(Object): pass
+
+class Symbol(Primitive): pass
+
+class Number(Primitive):
+    def __init__(self,N):
+        Primitive.__init__(self, float(N))
 
 class Container(Object): pass
 
 class Stack(Object): pass
 
 class Map(Container):
+    # map << obj
     def __lshift__(self,obj):
         if isinstance(obj,Object):
             self[obj.value] = obj
-        elif callable(objbye):
+        elif callable(obj):
             self << Fn(obj)
         else:
             raise SyntaxError(obj)
@@ -51,6 +80,7 @@ class Fn(Active):
 S = Stack('data')
 
 W = Map('vocabulary')
+
 W['W'] = W
 W['S'] = S
 
@@ -59,13 +89,17 @@ W << BYE
 
 import ply.lex as lex
 
-tokens = ['sym']
+tokens = ['symbol','number']
 
 t_ignore = ' \t\r\n'
 
-def t_sym(t):
+def t_number(t):
+    r'[\+\-]?[0-9]+(\.[0-9]*)?([eE][\+\-][0-9]+)?'
+    return Number(t.value)
+    
+def t_symbol(t):
     r'[0-9a-zA-Z_\.\?\+\-]+'
-    return t
+    return Symbol(t.value)
 
 def t_error(t): raise SyntaxError(t)
 
@@ -76,7 +110,7 @@ def INTERPRET(SRC):
     while True:
         token = lexer.token()
         if not token: break
-        print token
+        S << token
 
 while True:
     print W,S
