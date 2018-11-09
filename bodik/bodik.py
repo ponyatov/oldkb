8fbcece8046653a739f11994b637c9c84cea0d7e
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-09 11:58:25 +0400

    + parser
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- bodik/bodik.py
+++ bodik/bodik.py
@@ -1,2 +1,30 @@
 # https://www.youtube.com/playlist?list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8
 
+import ply.lex  as lex
+
+tokens = ['sym','num','op']
+
+t_ignore = ' \t\r\n'
+
+def t_op(t):
+    r'[\+\-\*\/\^\=]'
+    return t
+
+def t_num(t):
+    r'[0-9]+'
+    return t
+
+def t_sym(t):
+    r'[a-zA-Z0-9_]+'
+    return t
+
+def t_error(t): raise SyntaxError(t)
+
+lexer = lex.lex()
+
+while True:
+    lexer.input(raw_input('\nbodik> '))
+    while True:
+        token = lexer.token()
+        if not token: break
+        print token
