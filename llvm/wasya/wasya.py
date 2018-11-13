73a7de2d02a5131fd4c548b40f2cdd172d8429e0
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-13 15:50:51 +0400

    + wasya
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

---
+++ llvm/wasya/wasya.py
@@ -0,0 +1,8 @@
+# https://ponyatov.quora.com/Homebrew-WebAssembly-compiler
+
+# load LLVM binding library
+import llvmlite as ll
+
+# import PLY (Python Lex-Yacc) library (c) David M. Beazley
+import ply.lex  as lex
+import ply.yacc as yacc 
