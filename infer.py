9c62ae18edc5522fa767312aa13a855e28c21a64
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-15 17:13:44 +0400

    + links
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- infer.py
+++ infer.py
@@ -4,9 +4,17 @@
 ## @defgroup infer Logic
 ## @brief back-tracking logic inferense on trees/frames
 ## @details
-## * [CS164: Logic Programming Languages and Compilers](https://www.youtube.com/playlist?list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8)
 ## * [Marvin Minky's Frame system](https://www.youtube.com/watch?v=nXJ_2uGWM-M&index=3&list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8&t=24)
 ## * [YieldProlog](http://yieldprolog.sourceforge.net/)
+## * [Warren's Abstract Machine: A Tutorial Reconstruction](http://wambook.sourceforge.net/)
+## * [CS164: Logic Programming Languages and Compilers](https://www.youtube.com/playlist?list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8)
+##   * Lec.5 [Coroutines & Yield](https://www.youtube.com/watch?v=chJQC_3WUqg&index=9&list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8)
+##     Python already has `yield` implemented
+##   * Lec.6 [Intro to Prolog](https://www.youtube.com/watch?v=NT5RiJ6tgv8&list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8&index=9)
+##     **look it first**
+##     * [SWI Prolog online](https://swish.swi-prolog.org/) for test and learning
+##     * [look here first](https://swish.swi-prolog.org/p/CS_164_Prolog_intro_implementing.swinb)
+##       notebook with sample code and links for SWI Prolog (reference system)
  
 ## @{
 
