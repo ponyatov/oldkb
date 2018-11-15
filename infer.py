9ee2497236a860c5da57e63e46db67e3d6fb25ee
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-15 15:14:58 +0400

    + infer
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

---
+++ infer.py
@@ -0,0 +1,13 @@
+## @file
+## @brief back-tracking logic inferense
+
+## @defgroup infer Logic
+## @brief back-tracking logic inferense on trees/frames
+## @details
+## * [CS164: Logic Programming Languages and Compilers](https://www.youtube.com/playlist?list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8)
+## * [Marvin Minky's Frame system](https://www.youtube.com/watch?v=nXJ_2uGWM-M&index=3&list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8&t=24)
+## * [YieldProlog](http://yieldprolog.sourceforge.net/)
+ 
+## @{
+
+## @}
