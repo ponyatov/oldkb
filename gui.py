73c407f873876a95f94b6076fc5df62e11fbc668
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-15 10:31:33 +0400

    + file/save/quit
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- gui.py
+++ gui.py
@@ -12,11 +12,44 @@ import wx, wx.stc, threading
 from sym import *
 from forth import *
 
+import time
+
 ## generic GUI window
 class GUI_window(wx.Frame):
+    
+    ## @name initialization
+    
     ## define constructor parameters to default values specific for our app
-    def __init__(self,parent=None,title='KB/wx'):
-        wx.Frame.__init__(self,parent,title=title)
+    ## @param[in] title window title
+    ## @param[in] filename file name will be used on save console content
+    def __init__(self,parent=None,title='KB/wx',filename=sys.argv[0]+'.save'):
+        wx.Frame.__init__(self,parent,title='%s:%s'%(title,filename))
+        ## file name will be used for console content save/load
+        self.filename = filename
+        self.menu()
+    ## initialize menu
+    def menu(self):
+        ## menu bar
+        self.menubar = wx.MenuBar() ; self.SetMenuBar(self.menubar)
+        
+        ## file submenu
+        self.file = wx.Menu() ; self.menubar.Append(self.file,'&File')
+        ## file/save console content
+        self.save = self.file.Append(wx.ID_SAVE,'&Save')
+        self.Bind(wx.EVT_MENU,self.onSave,self.save)
+        ## file/quit
+        self.quit = self.file.Append(wx.ID_EXIT,'&Quit')
+        self.Bind(wx.EVT_MENU,self.onQuit,self.quit)
+        
+    ## @name event callbacks
+    
+    ## quit event callback
+    def onQuit(self,event): self.Close()
+    ## save console content callback
+    def onSave(self,event):
+        with open(self.filename,'a') as F:
+            print >>F,time.strftime('\n%H:%M:%S %d/%m/%Y',time.localtime()),
+            print >>F,self.GetTitle() 
 
 ## GUI works in a separate thread to let UI work in parallel with Forth VM
 class GUI_thread(threading.Thread):
