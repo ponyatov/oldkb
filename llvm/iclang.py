dcee3550e79f7de222fccb98de1387eb0dc59cc3
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-21 17:42:11 +0400

    + llvmlite llvm 6.0.1 / lite 0.26.0
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- llvm/iclang.py
+++ llvm/iclang.py
@@ -1,2 +1,20 @@
 # LLVM-backed interpreter
 
+# pip install --upgrade --user llvmlite
+
+# https://ian-bertolacci.github.io/llvm/llvmlite/python/compilers/programming/2016/03/06/LLVMLite_fibonacci.html
+from llvmlite import ir
+from llvmlite import binding as llvm
+
+llvm.initialize()
+llvm.initialize_all_targets()
+llvm.initialize_all_asmprinters()
+
+# print llvm.Target.from_triple('arm-unknown-unknown')
+
+module = ir.Module(name='fibonacci') ; print 'module',module
+
+i32 = ir.IntType(32) ; print 'i32',i32
+i64 = ir.IntType(64) ; print 'i64',i64
+
+fn_int_to_int = ir.FunctionType(i32,[i32]) ; print 'fn_int_to_int',fn_int_to_int
