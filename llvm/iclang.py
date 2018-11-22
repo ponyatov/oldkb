a309a38ef4bf0a30daa079bcf5085cff7b97cb80
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-22 14:58:08 +0400

    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- llvm/iclang.py
+++ llvm/iclang.py
@@ -6,19 +6,32 @@
 from llvmlite import ir
 from llvmlite import binding as llvm
 
+# ################################# module ####################################
+
 llvm.initialize()
-llvm.initialize_all_targets()
-llvm.initialize_all_asmprinters()
+# llvm.initialize_all_targets()
+# llvm.initialize_all_asmprinters()
+llvm.initialize_native_target()
+llvm.initialize_native_asmprinter()
 
 # target = llvm.Target.from_triple('wasm32') ; print 'target',target
-target = llvm.Target.from_triple('thumb') ; print 'target',target
+# target  = llvm.Target.from_triple('thumb-none-eabi') ; print 'target',target,target.triple
+target  = llvm.Target.from_default_triple() ; print 'target',target
+machine = target.create_target_machine() ; print 'machine',machine
 
-module = ir.Module(name='fibonacci') ; print 'module',module
+# JIT engine
+jit = llvm.create_mcjit_compiler(llvm.parse_assembly(''),machine) ; print 'jit',jit
 
-i32 = ir.IntType(32) ; print 'i32',i32
-# i64 = ir.IntType(64) ; print 'i64',i64
+module = ir.Module(name='fibonacci')
+
+# ################################# types #####################################
+void = ir.VoidType()
+float = ir.FloatType()
+i8,i16,i32,i64 = map(ir.IntType,[8,16,32,64])
 
 fn_int_to_int = ir.FunctionType(i32,[i32]) ; print 'fn_int_to_int',fn_int_to_int
+
+# ################################# parser ####################################
 
 import ply.lex  as lex
 import ply.yacc as yacc
@@ -36,6 +49,8 @@ def t_error(t): raise SyntaxError(t)
 
 lexer = lex.lex()
 
+# #############################################################################
+
 lexer.input('''
 
 print 1+2*3
@@ -45,3 +60,10 @@ while True:
     token = lexer.token()
     if not token: break
     print token
+
+# #############################################################################
+
+print '-'*77
+print module
+print llvm.parse_assembly(str(module))
+
