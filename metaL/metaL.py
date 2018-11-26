75a4f654b3092b6ff9dfb213cdbcd3ce2936b8ef
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-11-26 13:26:41 +0400

    + dup/swap and <<  operator
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- metaL/metaL.py
+++ metaL/metaL.py
@@ -11,8 +11,10 @@ class Obj:
     # print object
     def __repr__(self):
         return self.dump()
-    def dump(self,depth=0):
-        S = self.pad(depth) + self.head()
+    def dump(self,depth=0,prefix=''):
+        S = self.pad(depth) + self.head(prefix)
+        for i in self.attr: S += self.attr[i].dump(depth+1,prefix='%s = '%i)
+        for j in self.nest: S += j.dump(depth+1)
         return S
     def head(self,prefix=''):
         return '%s<%s:%s>' % (prefix, self.type, self.value)
@@ -22,12 +24,22 @@ class Obj:
     # attr{}    
     def __setitem__(self,key,obj): self.attr[key] = obj ; return self
     def __getitem__(self,key): return self.attr[key]
+    def __lshift__(self,obj):
+        if isinstance(obj,Obj): self[obj.value] = obj ; return self
+        if callable(obj): return self << Fn(obj)
+        raise TypeError(obj)
         
     # nest[] as stack
     def dropall(self): self.nest = []
     def push(self,obj): self.nest.append(obj) ; return self
     def pop(self): return self.nest.pop()
     def top(self): return self.nest[-1]
+    def dup(self): self.push(self.top())
+    def drop(self): self.pop()
+    def swap(self): B = self.pop() ; A = self.pop() ; self.push(B) ; self.push(A)
+    def over(self): self.push(self.nest[-2])
+    def press(self): del self.nest[-2]
+
     
 class Sym(Obj): pass
 class Str(Obj): pass
@@ -37,15 +49,24 @@ class Int(Num): pass
 class StacK(Obj): pass
 class Map(Obj): pass
 
+class Fn(Obj):
+    def __init__(self,F): Obj.__init__(self, F.__name__) ; self.fn = F
+    def __call__(self): self.fn()
+
 # stack
 S = StacK('data')
 
 # vocabulary
 W = Map('FORTH')
 
-W['?'] = W['WORDS'] = W.dump
+W['?'] = W['WORDS'] = Fn(W.dump)
 
-W['.'] = W['DROPALL'] = S.dropall 
+W['.'] = W['DROPALL'] = Fn(S.dropall)
+W['DUP']   = Fn(S.dup)
+W['DROP']  = Fn(S.drop)
+W['SWAP']  = Fn(S.swap)
+W['OVER']  = Fn(S.over)
+W['PRESS'] = Fn(S.press)
 
 import ply.lex as lex
 
@@ -68,14 +89,20 @@ def WORD():
     token = lexer.token()
     if not token: return False
     S.push(token) ; return True
+W << WORD
     
 def FIND():
     token = S.pop()
-    try: S.push(W[token.value]) ; return True
-    except KeyError: S.push(token) ; return False
+    try:
+        S.push(W[token.value]) ; return True
+    except KeyError:
+        try: S.push(W[token.value.upper()]) ; return True
+        except KeyError: S.push(token) ; return False
+W << FIND
     
 def EXECUTE():
     S.pop() ()
+W << EXECUTE
     
 def INTERPRET(SRC):
     lexer.input(SRC)
@@ -85,4 +112,3 @@ def INTERPRET(SRC):
 
 # REPL
 while True: print S ; INTERPRET(raw_input('\nok> '))
-                            
