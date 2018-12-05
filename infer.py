a14acfaab8c2b4163e016096e5a8bc7f142405f6
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-12-05 09:21:27 +0400

    + simpsons: male/female
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- infer.py
+++ infer.py
@@ -4,8 +4,8 @@
 ## @defgroup infer Logic
 ## @brief back-tracking logic inferense on trees/frames
 ## @details
+## * [**YieldProlog**](http://yieldprolog.sourceforge.net/)
 ## * [Marvin Minky's Frame system](https://www.youtube.com/watch?v=nXJ_2uGWM-M&index=3&list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8&t=24)
-## * [YieldProlog](http://yieldprolog.sourceforge.net/)
 ## * [Warren's Abstract Machine: A Tutorial Reconstruction](http://wambook.sourceforge.net/)
 ## * [CS164: Logic Programming Languages and Compilers](https://www.youtube.com/playlist?list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8)
 ##   * Lec.5 [Coroutines & Yield](https://www.youtube.com/watch?v=chJQC_3WUqg&index=9&list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8)
@@ -16,9 +16,28 @@
 ##     * [look here first](https://swish.swi-prolog.org/p/CS_164_Prolog_intro_implementing.swinb)
 ##       notebook with sample code and links for SWI Prolog (reference system)
 ##   * **Lec.7** [**real magic starts here**](https://www.youtube.com/watch?v=HKUL_iLYTQs&list=PLddc343N7YqgYvD0Kfc91QUcssH_5_sn8&index=11)
+## * Simpsons Prolog
+##   * https://threefiddyblog.wordpress.com/2017/06/25/prolog-logical-programming-and-the-simpsons/
  
 ## @{
 
+def male():
+    yield 'Homer'
+    yield 'Bart'
+    yield 'Abraham'
+    yield 'Skinner'
+    
+def female():
+    yield 'Marge'
+    yield 'Lisa'
+    yield 'Maggie'
+    yield 'Mona'
+    yield 'Jacqueline'
+    yield 'Paty'
+    yield 'Selma'
 
+for m in male(): print m
+print
+for f in female(): print f
 
 ## @}
