4b531899ff974bd6688eb7768c5a45ba90b6f3bd
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-12-13 17:22:00 +0400

    + generalUnify() + id(Var) in dump
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- infer.py
+++ infer.py
@@ -24,10 +24,12 @@
 # yield with 
 ## unifying logic variable
 class Var:
-    def __init__(self): self.value = None
+    def __init__(self,init=None): self.value = init
     def __repr__(self):
-        if self.value: return '<%s>' % self.value
-        else: return '<>'
+        
+        if self.value: R = str(self.value)
+        else: R=''
+        return '<%s> @%X' % (R,id(self))
     ## `<<` unifying operator
     def __lshift__(self,arg): return self.unify(arg)
     def unify(self,arg):
@@ -48,10 +50,39 @@ def female(P):
         for j in P << i:
             yield j
 
-P = Var() ; print 'P',P,
+P = Var() ; P.value = 'Marge'; print 'P',P,
 print ; print
 for i in male(P): print i,
 print ; print
 for j in female(P): print j,
+print ; print
+
+def generalGetValue(arg):
+    if isinstance(arg,Var):
+        if not arg.value: return arg    # unbound var as itself
+        else: return arg.value          # bound var.value
+    else: return arg                    # other objects as is
+
+def generalUnify(arg1, arg2):
+    print generalUnify
+    print 'args',arg1,arg2
+    arg1val = generalGetValue(arg1)
+    arg2val = generalGetValue(arg2)
+    print 'vals',arg1val,arg2val
+    if isinstance(arg1val, Var):        # unbound arg1
+        for i in arg1val << arg2val:
+            yield i
+    elif isinstance(arg2val, Var):      # unbound arg2
+        for j in arg2val << arg1val:
+            yield j
+    else:                               # both arg1/2 not Vars
+        if arg1val == arg2val:
+            yield Var(arg1val)
+    print 
+    
+for i in generalUnify( Var(), Var(2) ): print i
+for i in generalUnify(     2, Var()  ): print i
+for i in generalUnify( Var(), Var()  ): print i
+for i in generalUnify(     1, 1      ): print i
 
 ## @}
