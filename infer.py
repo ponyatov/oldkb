d0b9882200b8ba63e73d6e64747af307b00068e5
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-12-13 18:06:33 +0400

    + male/female via generalUnify
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- infer.py
+++ infer.py
@@ -40,49 +40,50 @@ class Var:
         elif self.value == arg: # == or bounded var is equals
             yield self          # return self unmodified
 
-def male(P):
-    for i in 'Homer Bart Abraham Skinner'.split():
-        for j in P << i:
-            yield j
-
-def female(P):
-    for i in 'Marge Lisa Maggie Paty Selma Mona Jacqueline'.split():
-        for j in P << i:
-            yield j
-
-P = Var() ; P.value = 'Marge'; print 'P',P,
-print ; print
-for i in male(P): print i,
-print ; print
-for j in female(P): print j,
-print ; print
-
 def generalGetValue(arg):
     if isinstance(arg,Var):
-        if not arg.value: return arg    # unbound var as itself
-        else: return arg.value          # bound var.value
-    else: return arg                    # other objects as is
+        if not arg.value: return arg        # unbound var as itself
+        else: return arg.value              # bound var.value
+    else: return arg                        # other objects as is
 
-def generalUnify(arg1, arg2):
-    print generalUnify
-    print 'args',arg1,arg2
+def generalUnify(arg1, arg2, debug=False):
+    if debug: print generalUnify
+    if debug: print 'args',arg1,arg2
     arg1val = generalGetValue(arg1)
     arg2val = generalGetValue(arg2)
-    print 'vals',arg1val,arg2val
-    if isinstance(arg1val, Var):        # unbound arg1
+    if debug: print 'vals',arg1val,arg2val
+    if isinstance(arg1val, Var):            # unbound arg1
         for i in arg1val << arg2val:
             yield i
-    elif isinstance(arg2val, Var):      # unbound arg2
+    elif isinstance(arg2val, Var):          # unbound arg2
         for j in arg2val << arg1val:
             yield j
-    else:                               # both arg1/2 not Vars
+    else:                                   # both arg1/2 not Vars
         if arg1val == arg2val:
             yield Var(arg1val)
-    print 
+    if debug: print 
     
 for i in generalUnify( Var(), Var(2) ): print i
 for i in generalUnify(     2, Var()  ): print i
 for i in generalUnify( Var(), Var()  ): print i
 for i in generalUnify(     1, 1      ): print i
+
+def male(P):
+    for i in 'Homer Bart Abraham Skinner'.split():
+        for j in generalUnify(P,i):
+            yield j
+
+def female(P):
+    for i in 'Marge Lisa Maggie Paty Selma Mona Jacqueline'.split():
+        for j in generalUnify(P,i):
+            yield j
+
+P = Var() #; P.value = 'Marge'; 
+print 'P',P,
+print ; print
+for i in male(P): print i,
+print ; print
+for j in female(P): print j,
+print ; print
 
 ## @}
